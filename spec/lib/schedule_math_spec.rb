49e875b79fbf69cdd7c14a7d8f37e0e1e79374ec
Andy Lindeman <andy@highgroove.com>
2011-03-02 17:27:00 -0600

    Add tests for ScheduleMath#remaining

---
+++ spec/lib/schedule_math_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe ScheduleMath do
+  subject do
+    Class.new { include ScheduleMath }.new
+  end
+
+  describe "#remaining" do
+    it "returns the difference between the number of hours completed and the budget" do
+      subject.stub(:budget).and_return(100)
+      subject.stub(:completed).and_return(25)
+
+      subject.remaining.should == 75
+    end
+
+    it "returns 0 if the number of completed hours exceeds the budget" do
+      subject.stub(:budget).and_return(100)
+      subject.stub(:completed).and_return(125)
+
+      subject.remaining.should == 0
+    end
+  end
+end

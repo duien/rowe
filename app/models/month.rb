69d4e4f918326fb205e1f69e080882ffcaab3da0
Emily Price <price.emily@gmail.com>
2011-04-11 15:22:01 -0400

    Include 'other' in month's completed
    [#10232601]

--- app/models/month.rb
+++ app/models/month.rb
@@ -19,7 +19,7 @@ class Month
   many :projects
 
   def completed
-    self.projects.select{ |p| p.billable or p.budget > 0 }.map(&:completed).inject(&:+)
+    ( self.projects.select{ |p| p.billable or p.budget > 0 }.map(&:completed).inject(&:+) or 0 ) + other.completed
   end
 
   def start_day
---
+++ spec/models/month_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe Month do
+  describe "#completed" do
+    context "with billable project" do
+      let(:budgeted_project)  { Project.new.stub(:completed => 5,
+                                                 :billable  => true,
+                                                 :budget    => 3) }
+      let(:unbudgeted_project){ Project.new.stub(:completed => 5,
+                                                 :billable  => true,
+                                                 :budget    => 0) }
+      it "should include project with budget > 0" do
+        month = Month.new( :projects => [ budgeted_project ] ) 
+        month.completed.should == 5
+      end
+
+      it "should include project with 0 budget" do
+        month = Month.new( :projects => [ unbudgeted_project ] ) 
+        month.completed.should == 5
+      end
+    end
+
+    context "with non-billable project" do
+      let(:budgeted_project)  { Project.new.stub(:completed => 5,
+                                                 :billable  => false,
+                                                 :budget    => 3) }
+      let(:unbudgeted_project){ Project.new.stub(:completed => 5,
+                                                 :billable  => false,
+                                                 :budget    => 0) }
+      it "should include project with budget > 0" do
+        month = Month.new( :projects => [ budgeted_project ] ) 
+        month.completed.should == 5
+      end
+
+      it "should not include project with 0 budget" do
+        month = Month.new( :projects => [ unbudgeted_project ] ) 
+        month.completed.should == 0
+      end
+    end
+
+    context "other" do
+      it "should include other" do
+        month = Month.new( :projects => [ ],
+                           :other    => Project.new.stub(:completed => 5) )
+        month.completed.should == 5
+      end
+    end
+  end
+end
+      

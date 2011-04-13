d0800e868f59c6587836c50da874fac1ed1c0e0c
Emily Price <price.emily@gmail.com>
2011-04-13 15:18:48 -0400

    Remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rowe::Application.routes.draw do
   root :to => 'dashboard#index'
-  match '/manual'   => 'dashboard#manual_index'
   get   '/settings' => 'settings#settings', :as => 'settings'
   put   '/settings' => 'settings#update', :as => 'update_settings'
   devise_for :users
